d9d200b8aba834ece18b70696307fbdf64ca613d
fabricio <fabricxx@gmail.com>
2020-07-18 02:29:31 -0300

    criado tela de login

---
+++ chatFirebase/app/src/main/java/com/fabricio/chatfirebase/MainActivity.java
@@ -0,0 +1,53 @@
+package com.fabricio.chatfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText meditemail;
+    private EditText meditpassword;
+    private Button mbuttonEnter;
+    private TextView mtxtaccount;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        meditemail = findViewById(R.id.editEmail);
+        meditpassword = findViewById(R.id.editPassword);
+        mbuttonEnter = findViewById(R.id.btn_Enter);
+        mtxtaccount = findViewById(R.id.txt_account);
+
+        mbuttonEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                //testar no logcat se esta aparecendo o email e senha quando digita
+                String email = meditemail.getText().toString();
+                String pass = meditpassword.getText().toString();
+                Log.i("teste",email);
+                Log.i("teste",pass);
+                
+            }
+        });
+
+
+    }
+
+
+
+
+
+
+}
